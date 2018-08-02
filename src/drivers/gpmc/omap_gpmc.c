4e027ef7bcf6a6fbc7254c6caa7eedb41015cdc2
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-08-02 14:25:34 +0300

    drivers: Improve omap_gpmc.c

--- src/drivers/gpmc/omap_gpmc.c
+++ src/drivers/gpmc/omap_gpmc.c
@@ -17,8 +17,9 @@
 #include <mem/vmem.h>
 
 #include <drivers/omap_gpmc.h>
+#include <drivers/gpmc.h>
 
-#define GPMC_MEMORY_START  0x00000000
+#define GPMC_MEMORY_START  0x00000000 /*0x1000000*/
 #define GPMC_MEMORY_END    0x3fffffff
 #define GPMC_CS_MEMORY_MAX 0x01000000
 
@@ -27,6 +28,23 @@
 
 
 EMBOX_UNIT_INIT(gpmc_init);
+
+
+unsigned long gpmc_mem_align(unsigned long size)
+{
+	int order;
+
+	size = (size - 1) >> (GPMC_CHUNK_SHIFT - 1);
+	order = GPMC_CHUNK_SHIFT - 1;
+	do {
+		size >>= 1;
+		order++;
+	} while (size);
+	size = 1 << order;
+	return size;
+}
+
+
 
 uint32_t gpmc_reg_read(int offset) {
 	return REG_LOAD(GPMC_BASE_ADDRESS + offset);
@@ -47,13 +65,12 @@ void gpmc_cs_reg_write(int cs, int offset, uint32_t val) {
 	reg_addr = GPMC_BASE_ADDRESS + offset + (cs * GPMC_CS_SIZE);
 	REG_STORE(reg_addr, val);
 }
-#if 0
-static int gpmc_cs_enabled(int cs) {
+
+int gpmc_cs_enabled(int cs) {
 	uint32_t l;
 	l = gpmc_cs_reg_read(cs, GPMC_CS_CONFIG7);
 	return l & GPMC_CONFIG7_CSVALID;
 }
-#endif
 
 static int gpmc_cs_enable_mem(int cs, uint32_t base, uint32_t size) {
 	uint32_t l;
